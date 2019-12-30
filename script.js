b173a07134ef7903a6684ada60d866bed97c1e03
quentingz <guez.quentin@gmail.com>
2019-12-30 01:17:42 +0100

    Première tentative pour arrêter la balle au moment où elle touche l'ellipse

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 
 var responsiveConst = 205;
-var a = 500, b = 300;
+var a = 500, b = 300, r = 5; // a = largeur de l'ellipse, b = hauteur de l'ellipse, r = rayon de la balle
 
 canvas.width = document.body.clientWidth - innerWidth / responsiveConst;
 canvas.height = document.body.clientHeight - innerHeight / responsiveConst;
@@ -38,27 +38,96 @@ function drawMyOwnEllipse() {
 
 // equation de l'ellipse : y = +/- sqrt(b²(1-x²/a²))
 
-var xB = canvas.width / 2 - 500 + 30, pos = true, yB = canvas.height / 2;
-var deplacement = 4;
+var xB, yB, deplacementX, deplacementY, stop = false, lance = false, oldX, oldY;
+
+function lancer() {
+    xB = canvas.width / 2 - a + r + r, yB = canvas.height / 2;
+    deplacementX = Math.random() * 5 + 3, deplacementY = Math.random() * 10 -5; // Math.random() * 10 - 5
+    console.log("deplacement x : " + deplacementX + "\ndeplacement y : " + deplacementY + "\n*****************\n\n")
+    stop = false;
+    deplacerX();
+}
+
+addEventListener('keydown', function (e) {
+    if (e.keyCode === 32 && lance) {
+        stop = true;
+        lance = false;
+    }else if (e.keyCode === 32) {
+        stop = false;
+        lancer();
+        lance = true;
+    }
+});
 
 function deplacerX() {
-    if (xB >= canvas.width / 2 + 500 - 30)
-        pos = false;
-    if (xB <= canvas.width / 2 - 500 + 30)
-        pos = true;
+    if (stop)
+        return;
+    if (Math.floor(Math.sqrt(Math.pow(b, 2) * (1 -
+        Math.pow(xB - (canvas.width / 2 - a) - a + r, 2) /
+        Math.pow(a, 2)))) === Math.abs(yB - (canvas.height / 2 - b) - b))
+        console.log(xB);
+
+    if (xB >= canvas.width / 2 + a - r && (Math.abs(yB - (canvas.height / 2 - b) - b)) === Math.sqrt(Math.pow(b, 2) * (1 -
+     Math.pow(xB - (canvas.width / 2 - a) - a + r, 2) /
+     Math.pow(a, 2)))) {
+        deplacementX *= -1;
+    }
+
+    var z = xB - (canvas.width / 2 - a) - a;
+    var k = (yB - (canvas.height / 2 - b) - b) * -1;
+
+    oldX = z - deplacementX;
+    oldY = k + deplacementY;
+
+    var m = (oldX - z) / (oldY - k);
+    var p = k - m * z;
+    var yDebut = Math.abs(Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z - r, 2) / Math.pow(a, 2))));
+    var yFin = Math.abs(Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z + r, 2) / Math.pow(a, 2))));
+
+    console.log("y de la balle pour le bout de la balle :" + Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z + r, 2) / Math.pow(a, 2))));
+    console.log("y de la balle pour le début de la balle :" + Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z - r, 2) / Math.pow(a, 2))));
+    console.log("y actuel : " + (k + r));
+    //console.log(k + r >= Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z - r, 2) / Math.pow(a, 2))) && k + r <=
+    // Math.sqrt(Math.pow(b, 2) * (1 - Math.pow(z + r, 2) / Math.pow(a, 2))));
+    if (k > 0) {
+        if (z < 0) {
+            if (Math.abs(k + r) >= yDebut && k + r <= yFin) {
+                stop = true;
+            }
+        }else {
+            if (k + r <= yDebut && k + r >= yFin) {
+                stop = true;
+            }
+        }
+    }else {
+        if (z < 0) {
+            if (k - r <= -yDebut && k - r >= -yFin) {
+                stop = true;
+            }
+        }else {
+            if (k - r >= -yDebut && k - r <= -yFin) {
+                stop = true;
+            }
+        }
+    }
+
+    if (k > b || k < -b || z > a) {
+        alert("Perdu !");
+        stop = true;
+    }
+
+    //console.log("equation de la droite : y=" + m + "x + " + p);
+
     requestAnimationFrame(deplacerX);
     drawEllipse();
     ctx.fillStyle = "green";
     ctx.beginPath();
-    ctx.arc(xB, yB, 30, -Math.PI, 2*Math.PI);
+    ctx.arc(xB, yB, r, -Math.PI, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
-    if (pos) {
-        xB += deplacement;
-    }else {
-        xB -= deplacement;
-    }
+    xB += deplacementX;
+    yB += deplacementY;
 }
 
 /*drawEllipse();
@@ -66,6 +135,4 @@ function deplacerX() {
  */
 
 
-
-drawMyOwnEllipse();
-deplacerX();
+drawEllipse();
